f0d727d54a5e63fa9b64b92e484d33998ca8a13c
Sindri Thor <sindrihar@hotmail.com>
2020-09-20 18:30:49 +0000

    Project 5: Math Functions

--- math_functions.py
+++ math_functions.py
@@ -1,5 +1,11 @@
 import math
 
+NATURAL = "a"
+FIBONACCI = "b"
+EULER = "c"
+EXIT = "x"
+
+# Prentar Valmynd
 def valmynd():
     print("Please choose one of the options below:")
     print("a. Display the sum of the first N natural numbers. ")
@@ -9,55 +15,58 @@ def valmynd():
     print()
     return
 
-
+# Athugar hvort notandi hafi slegið inn löglegt input
 def isallowed(userinput):
-    if(userinput == "a"):
+    if(userinput == NATURAL):
         return True
-    elif(userinput == "b"):
+    elif(userinput == FIBONACCI):
         return True
-    elif(userinput == "c"):
+    elif(userinput == EULER):
         return True
     return False
 
-
+# Býr til streng til þess að prenta út frá reikniföllum
 def getoutstring(selected,inputnumber):
 
-    if(selected == "a"):
+    if(selected == NATURAL):
         outputstring = "Natural number sum: {}".format(sum_natural(inputnumber))
-    elif(selected == "b"):
+    elif(selected == FIBONACCI):
         outputstring = "Fibonacci sum: {}".format(sum_fibonacci(inputnumber))
     else:
          outputstring = "Euler approximation: {}".format(approximate_euler(inputnumber))
 
     return outputstring
     
+# Reiknifall náttúrulega talna
+def sum_natural(n_str):
 
-
-def sum_natural(n_int):
-
+    n_int = int(n_str)
     if n_int >= 2:
         result = int(n_int*(n_int+1) / 2)
         return result
 
+# Reinkifall Fibonacci 
+def sum_fibonacci(n_str):
 
-
-def sum_fibonacci(n_int):
-
+    n_int = int(n_str)
     if n_int >= 2:
 
-        fib_int_1 = 0
-        fib_int_2 = 1
+        fibonacci_int_1 = 0
+        fibonacci_int_2 = 1
         result = 1
 
         for i in range(n_int-2):
-            fib_int_3 = fib_int_1 + fib_int_2
-            fib_int_1 = fib_int_2
-            fib_int_2 = fib_int_3
-            result += fib_int_3
+            fibonacci_int_3 = fibonacci_int_1 + fibonacci_int_2
+            fibonacci_int_1 = fibonacci_int_2
+            fibonacci_int_2 = fibonacci_int_3
+            result += fibonacci_int_3
 
         return result
 
-def approximate_euler(n_int):
+# Reiknifall Euler
+def approximate_euler(n_str):
+
+    n_int = int(n_str)
     if n_int >= 2:
         result = 1
 
@@ -67,18 +76,20 @@ def approximate_euler(n_int):
         return round(result,5)
 
 
+# Aðal keyrsla forritsins
+
 option = ""
 valmynd()
-while option != "x":
-    
+while option != EXIT:
+
     option = input("Enter option: ").lower()
 
     if isallowed(option):
-        number = int(input("Enter N: "))
+        number = input("Enter N: ")
         outstring = getoutstring(option,number)
         print(outstring)
         print()
 
-    elif(option != "x"):
+    elif(option != EXIT):
         print("Unrecognized option ",option)
         valmynd()
